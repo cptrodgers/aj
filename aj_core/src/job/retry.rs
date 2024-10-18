7cf5a9f4a07e21993fed0b0bc7b6999686549786
cptrodgers <cptrodgers@gmail.com>
2024-10-18 18:13:48 +0700

    Add Retry Exponential Backoff stategy

--- aj_core/src/job/retry.rs
+++ aj_core/src/job/retry.rs
@@ -7,7 +7,11 @@ use crate::util::get_now;
 #[serde_as]
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum RetryStrategy {
+    /// Retry the job with consistent time. Example: 2s, 4s, 6s, 8s ...
     Interval(#[serde_as(as = "serde_with::DurationMicroSeconds<i64>")] Duration), // ms
+    /// Retry the job with exponential backoff time. Example: 1s, 2s, 4s, 8s, 16s ...
+    /// This stratgy is a good fit retry solution if you fail to access http request with rate limit.
+    ExponentialBackoff(#[serde_as(as = "serde_with::DurationMicroSeconds<i64>")] Duration), // ms
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -37,6 +41,11 @@ impl Retry {
         Self::new(0, max_retries, RetryStrategy::Interval(interval))
     }
 
+    pub fn new_exponential_backoff(max_retries: Option<i32>, initial_backoff: Duration) -> Self {
+        let strategy = RetryStrategy::ExponentialBackoff(initial_backoff);
+        Self::new(0, max_retries, strategy)
+    }
+
     pub fn should_retry(&self) -> bool {
         if let Some(max_retries) = self.max_retries {
             self.retried_times < max_retries
@@ -49,7 +58,8 @@ impl Retry {
         self.retried_times += 1;
         let final_now = now.unwrap_or(get_now());
         match &self.strategy {
-            RetryStrategy::Interval(ms) => final_now + *ms,
+            RetryStrategy::Interval(internal_time) => final_now + *internal_time,
+            RetryStrategy::ExponentialBackoff(initial_backoff) => final_now + *initial_backoff * 2_i32.pow(self.retried_times as u32),
         }
     }
 }
@@ -74,9 +84,28 @@ mod tests {
 
         match retry.strategy {
             RetryStrategy::Interval(duration) => {
-                assert_eq!(duration, Duration::try_milliseconds(100).unwrap())
+                assert_eq!(duration, Duration::try_milliseconds(100).unwrap());
             }
+            _ => panic!("Default Retry should use Internal strategy"),
         }
+    }
+
+    #[test]
+    fn test_exponential_backoff_retry() {
+        let initial_backoff = Duration::seconds(5);
+        let now = mock_now();
+        let max_retries = 3;
+
+        let mut retry = Retry::new_exponential_backoff(Some(max_retries), initial_backoff);
+
+        for retry_times in 1..=max_retries {
+            assert!(retry.should_retry());
+
+            let first_retry = retry.retry_at(Some(now));
+            assert_eq!(now + initial_backoff * 2_i32.pow(retry_times as u32), first_retry);
+        }
+
+        assert!(!retry.should_retry());
     }
 
     #[test]
